40c1df047cebcc4d5f2c633a99e399e2d9a2be14
Vitor Cardim <vcm3@cin.ufpe.br>
2020-10-13 18:44:59 -0300

    DB Name correction

--- sql/CreateDB.sql
+++ sql/CreateDB.sql
@@ -70,14 +70,13 @@ INSERT INTO
     Genres(Label,Created,Updated)
 VALUES ('Suspense',GETDATE(),GETDATE())
 
-
 CREATE TABLE [dbo].[Person] (
 	[CPF] [char](11) NOT NULL,
-    [Name] [varchar](200) NOT NULL,
+    [Name] [varchar](50) NOT NULL,
     [Email] [varchar](50) NOT NULL,
     [Age] [varchar](3) NOT NULL,
-    [Phoner Number] [varchar](15) NOT NULL, /* Because of international Numbers*/
-    [Password] [varchar](20) NOT NULL,
+    [PhoneNumber] [varchar](15) NOT NULL, /* Because of international Numbers*/
+    [Password] [varchar](250) NOT NULL,
     [ProfileId] [int] NOT NULL,
     [Fee] [Decimal],
 	[Created] DateTime NOT NULL,
@@ -85,7 +84,6 @@ CREATE TABLE [dbo].[Person] (
     CONSTRAINT PK_Person_CPF PRIMARY KEY CLUSTERED (CPF),
     CONSTRAINT FK_Person_ProfileId FOREIGN KEY (ProfileId) REFERENCES [dbo].[Profile] (Id)
     )
-
 
 CREATE TABLE [dbo].[PersonGenres] (
 	[Id] [int] IDENTITY(1,1) NOT NULL,
@@ -102,7 +100,7 @@ CREATE TABLE [dbo].[Production] (
     [Name] [varchar](200) NOT NULL,
     [Created] DateTime NOT NULL,
     [Updated] DateTime NOT NULL,
-    CONSTRAINT PK_Production_Id PRIMARY KEY CLUSTERED (Id),
+    CONSTRAINT PK_Production_Id PRIMARY KEY CLUSTERED (Id)
 )
 
 CREATE TABLE [dbo].[Reservation] (
@@ -113,7 +111,6 @@ CREATE TABLE [dbo].[Reservation] (
     [InitalDate] Date NOT NULL,
     [FinalDate] Date NOT NULL,
     CONSTRAINT PK_Reservation_Id PRIMARY KEY CLUSTERED (Id),
-    CONSTRAINT FK_PersonGenres_CPF FOREIGN KEY (CPF) REFERENCES [dbo].[Person] (CPF),
-    CONSTRAINT FK_PersonGenres_ProductionId FOREIGN KEY (ProductionId) REFERENCES [dbo].[Production] (id)
+    CONSTRAINT FK_Reservation_CPF FOREIGN KEY (CPF) REFERENCES [dbo].[Person] (CPF),
+    CONSTRAINT FK_Reservation_ProductionId FOREIGN KEY (ProductionId) REFERENCES [dbo].[Production] (id)
 )
-

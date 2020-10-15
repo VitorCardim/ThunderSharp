0f59f0da07e3a9a5186794d6c2ec0d671298f9cd
Gerson Brandão <gerson.leitebrandao@gmail.com>
2020-10-15 08:46:02 -0300

    Finalizando ProductionRepository

---
+++ ThunderSharpAPI/Thunder.Application/AppThunder/Interfaces/IProductionAppServices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Thunder.Application.AppThunder.Input;
+using Thunder.Domain.Entities;
+
+namespace Thunder.Application.AppThunder.Interfaces
+{
+    public interface IProductionAppServices
+    {
+        Task<Production> Insert(ProductionInput production);
+        Task<Production> GetByID(int id);
+        IEnumerable<Production> Get();
+    }
+}

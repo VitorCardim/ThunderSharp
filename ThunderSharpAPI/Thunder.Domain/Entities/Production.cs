477de60473a61012c24f524f6f3e61644e95d019
Gerson Brandão <gerson.leitebrandao@gmail.com>
2020-10-14 22:44:13 -0300

    Create class Production

---
+++ ThunderSharpAPI/Thunder.Domain/Entities/Production.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Thunder.Domain.Interfaces.Repository;
+
+namespace Thunder.Domain.Entities
+{
+    public class Production 
+
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        //AJUSTAR - para o tipo correto do CPF
+        public string CPF { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Update { get; set; }
+
+        //AJUSTAR - para o tipo correto do CPF
+        public Production(Guid id, string name, string cpf, DateTime created)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.CPF = cpf;
+            this.Created = DateTime.Now;
+
+        }
+
+
+    }
+}
---
+++ ThunderSharpAPI/Thunder.Infrastructure.Repositories/ProductionRepository.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using Thunder.Domain.Entities;
+using Thunder.Domain.Interfaces.Repository;
+
+namespace Thunder.Infrastructure.Repositories
+{
+    class ProductionRepository : IProduction
+    {
+        private readonly IConfiguration _configuration;
+        public ProductionRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IEnumerable<Production> Get()
+        {
+
+            throw new NotImplementedException();
+
+        }
+
+        public Task<Production> GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(Production production)
+        {
+            try
+            {
+                using var con = new SqlConnection(_configuration["ConnectionString"]);
+                var sqlCmd = @"INSERT INTO 
+                                  PRODUCER (FullName, 
+                                        Email, 
+                                        PhoneNumber, 
+                                        Age,
+                                        Password) 
+                               VALUES (@FullName, 
+                                        @Email,
+                                        @Age, 
+                                        @Password);";
+
+                using SqlCommand cmd = new SqlCommand(sqlCmd, con)
+                {
+                    CommandType = CommandType.Text
+                };
+
+                cmd.Parameters.AddWithValue("FullName", "");
+                cmd.Parameters.AddWithValue("Email", "");
+                cmd.Parameters.AddWithValue("Age", "");
+                cmd.Parameters.AddWithValue("Password", "");
+
+                await con.OpenAsync();
+                var id = cmd.ExecuteScalar();
+
+                return int.Parse(id.ToString());
+            }
+
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+    }
+}

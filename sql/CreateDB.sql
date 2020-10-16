f6f2b50856a181cc9dcbe378dcd5962812b8cf33
Vitor Cardim <vcm3@cin.ufpe.br>
2020-10-16 00:38:01 -0300

    DB Population

--- sql/CreateDB.sql
+++ sql/CreateDB.sql
@@ -61,6 +61,34 @@ CREATE TABLE [dbo].[Person] (
     CONSTRAINT FK_Person_ProfileId FOREIGN KEY (ProfileId) REFERENCES [dbo].[Profile] (Id)
     )
 
+INSERT INTO 
+    Person(Name,Email,Age,PhoneNumber,Password,ProfileId,Fee,Created,Updated)
+VALUES ('ProdutorOne','Produtor@gmail.com','50','999999999','qwasdsadsad',1,null,Getdate(),Getdate())
+
+INSERT INTO 
+    Person(Name,Email,Age,PhoneNumber,Password,ProfileId,Fee,Created,Updated)
+VALUES ('ActorOne','ActorOne@gmail.com','22','999999999','qwasdsadsad',2,1231,Getdate(),Getdate())
+
+INSERT INTO 
+    Person(Name,Email,Age,PhoneNumber,Password,ProfileId,Fee,Created,Updated)
+VALUES ('ActorTwo','ActorTwo@gmail.com','21','999999999','qwasdsadsad',2,311,Getdate(),Getdate())
+
+INSERT INTO 
+    Person(Name,Email,Age,PhoneNumber,Password,ProfileId,Fee,Created,Updated)
+VALUES ('ActressOne','ActressOne@gmail.com','19','999999999','qwasdsadsad',2,12331,Getdate(),Getdate())
+
+INSERT INTO 
+    Person(Name,Email,Age,PhoneNumber,Password,ProfileId,Fee,Created,Updated)
+VALUES ('ActressTwo','ActressTwo@gmail.com','30','999999999','qwasdsadsad',2,12331,Getdate(),Getdate())
+
+INSERT INTO 
+    Person(Name,Email,Age,PhoneNumber,Password,ProfileId,Fee,Created,Updated)
+VALUES ('Nome1','Nome@gmail.com','19','999999999','qwasdsadsad',2,12331,Getdate(),Getdate())
+
+INSERT INTO 
+    Person(Name,Email,Age,PhoneNumber,Password,ProfileId,Fee,Created,Updated)
+VALUES ('ProductorTwo','ProductorTwo@gmail.com','19','999999999','qwasdsadsad',1,Null,Getdate(),Getdate())
+
 CREATE TABLE [dbo].[PersonGenres] (
 	[Id] [int] IDENTITY(1,1) NOT NULL,
     [PersonId] [int] NOT NULL,
@@ -70,6 +98,34 @@ CREATE TABLE [dbo].[PersonGenres] (
     CONSTRAINT FK_PersonGenres_PersonId FOREIGN KEY (PersonId) REFERENCES [dbo].[Person] (Id),
     CONSTRAINT FK_PersonGenres_GenreId FOREIGN KEY (GenreId) REFERENCES [dbo].[Genres] (id)
 )
+
+INSERT INTO 
+    PersonGenres(PersonId,GenreId,Created)
+VALUES (2,2,Getdate())
+
+INSERT INTO 
+    PersonGenres(PersonId,GenreId,Created)
+VALUES (2,1,Getdate())
+
+INSERT INTO 
+    PersonGenres(PersonId,GenreId,Created)
+VALUES (2,3,Getdate())
+
+INSERT INTO 
+    PersonGenres(PersonId,GenreId,Created)
+VALUES (3,1,Getdate())
+
+INSERT INTO 
+    PersonGenres(PersonId,GenreId,Created)
+VALUES (4,2,Getdate())
+
+INSERT INTO 
+    PersonGenres(PersonId,GenreId,Created)
+VALUES (5,3,Getdate())
+
+INSERT INTO 
+    PersonGenres(PersonId,GenreId,Created)
+VALUES (6,1,Getdate())
 
 CREATE TABLE [dbo].[Production] (
 	[Id] [int] IDENTITY(1,1) NOT NULL,
@@ -81,14 +137,89 @@ CREATE TABLE [dbo].[Production] (
     CONSTRAINT FK_Production_PersonId FOREIGN KEY (PersonId) REFERENCES [dbo].[Person] (Id),
 )
 
+
+INSERT INTO 
+    Production(Name,PersonId,Created,Updated)
+VALUES ('O vento nos levou',1,Getdate(),Getdate())
+
+INSERT INTO 
+    Production(Name,PersonId,Created,Updated)
+VALUES ('O vento te levou',1,Getdate(),Getdate())
+
+INSERT INTO 
+    Production(Name,PersonId,Created,Updated)
+VALUES ('O Sono nos levou',2,Getdate(),Getdate())
+
+
+
 CREATE TABLE [dbo].[Reservation] (
 	[Id] [int] IDENTITY(1,1) NOT NULL,
     [PersonId] [int] NOT NULL,
 	[ProductionId] [int] NOT NULL,
 	[Created] DateTime NOT NULL,
-    [InitalDate] Date NOT NULL,
+    [InitialDate] Date NOT NULL,
     [FinalDate] Date NOT NULL,
     CONSTRAINT PK_Reservation_Id PRIMARY KEY CLUSTERED (Id),
     CONSTRAINT FK_Reservation_PersonId FOREIGN KEY (PersonId) REFERENCES [dbo].[Person] (Id),
     CONSTRAINT FK_Reservation_ProductionId FOREIGN KEY (ProductionId) REFERENCES [dbo].[Production] (id)
 )
+
+
+INSERT INTO 
+    Reservation(PersonId,ProductionId,Created,InitialDate,FinalDate)
+VALUES (2,1,Getdate(),DateAdd(dd,2,GetDate()),DateAdd(dd,3,GetDate()))
+
+INSERT INTO 
+    Reservation(PersonId,ProductionId,Created,InitialDate,FinalDate)
+VALUES (2,2,Getdate(),DateAdd(dd,4,GetDate()),DateAdd(dd,5,GetDate()))
+
+INSERT INTO 
+    Reservation(PersonId,ProductionId,Created,InitialDate,FinalDate)
+VALUES (3,1,Getdate(),DateAdd(dd,2,GetDate()),DateAdd(dd,3,GetDate()))
+
+INSERT INTO 
+    Reservation(PersonId,ProductionId,Created,InitialDate,FinalDate)
+VALUES (4,1,Getdate(),DateAdd(dd,2,GetDate()),DateAdd(dd,2,GetDate()))
+
+INSERT INTO 
+    Reservation(PersonId,ProductionId,Created,InitialDate,FinalDate)
+VALUES (5,1,Getdate(),DateAdd(dd,10,GetDate()),DateAdd(dd,12,GetDate()))
+
+INSERT INTO 
+    Reservation(PersonId,ProductionId,Created,InitialDate,FinalDate)
+VALUES (6,1,Getdate(),DateAdd(dd,1,GetDate()),DateAdd(dd,10,GetDate()))
+
+INSERT INTO 
+    Reservation(PersonId,ProductionId,Created,InitialDate,FinalDate)
+VALUES (6,1,Getdate(),DateAdd(dd,20,GetDate()),DateAdd(dd,40,GetDate()))
+
+INSERT INTO 
+    Reservation(PersonId,ProductionId,Created,InitialDate,FinalDate)
+VALUES (6,1,Getdate(),DateAdd(dd,15,GetDate()),DateAdd(dd,19,GetDate()))
+
+
+
+/* Selects
+
+
+select *
+From Reservation
+
+Select *
+From Profile
+
+Select *
+From Genres
+
+Select *
+From Person
+
+Select *
+From PersonGenres
+
+Select *
+From Production
+
+Select *
+From Reservation
+*/
